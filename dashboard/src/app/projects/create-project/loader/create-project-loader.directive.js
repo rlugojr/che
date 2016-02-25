1c0f647136a55e3530b5ffd2393fb99bc874c852
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2016-02-25 16:29:22 +0200

    add terminal; use in create project;

--- dashboard/src/app/projects/create-project/loader/create-project-loader.directive.js
+++ dashboard/src/app/projects/create-project/loader/create-project-loader.directive.js
@@ -19,13 +19,67 @@ export class CreateProjectLoader {
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor () {
+  constructor ($q) {
+    this.$q = $q;
     this.restrict = 'E';
     this.templateUrl = 'app/projects/create-project/loader/create-project-loader.html';
 
     // scope values
     this.scope = {
-      step: '=cheStep'
+      model: '=cheModel'
     };
+  }
+
+  link ($scope, element) {
+    let craneEl = element.find('.create-project-loader'),
+      cargoEl = element.find('#load'),
+      workingLogEl = element.find('.working-log'),
+      oldStep, newStep;
+    // $scope.$watch(() => {return $scope.step;}, (newVal,oldVal) => {
+    $scope.$watch(() => {return $scope.model.getCurrentProgressStep();}, (newVal,oldVal) => {
+
+      oldStep = oldVal;
+      newStep = newVal;
+
+      if (oldVal===newVal) {
+        let step = 1;
+        if (newVal>0){
+          step = newVal;
+        }
+        craneEl.addClass('step-1');
+        cargoEl.addClass('layer-1');
+        return;
+      }
+      let stages = element.find('.stage'),
+        outputs = stages.parent();
+
+      stages.removeClass('active');
+      outputs.find('.stage-' + newVal).addClass('active');
+
+      if (workingLogEl.hasClass('untouched')) {
+        stages.removeClass('opened');
+        outputs.find('.stage-' + newVal).addClass('opened');
+      }
+    });
+
+    element.find('.anim.trolley-block').bind('animationiteration', () => {
+      if (newStep && oldStep){
+        craneEl.removeClass('step-'+oldStep).addClass('step-'+newStep);
+        cargoEl.removeClass('layer-'+oldStep).addClass('layer-'+newStep);
+      }
+    });
+
+    // manual switching by stages
+    workingLogEl.bind('click', (event) => {
+      let targetEl = angular.element(event.target);
+      if (!targetEl.hasClass('stage-title')) {
+        return false;
+      }
+      let selectedStageEl = targetEl.parent(),
+        stages = workingLogEl.find('.stage');
+      workingLogEl.removeClass('untouched');
+      stages.removeClass('opened');
+      selectedStageEl.addClass('opened');
+    });
   }
 }
