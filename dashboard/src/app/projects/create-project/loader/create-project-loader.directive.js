261a408324dc87ff263c4dfccaa5b9bde105d60e
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2016-02-26 18:11:32 +0200

    replace creating project loader Signed-off-by: Oleksii Kurinnyi <okurinnyi@codenvy.com>

--- dashboard/src/app/projects/create-project/loader/create-project-loader.directive.js
+++ dashboard/src/app/projects/create-project/loader/create-project-loader.directive.js
@@ -25,7 +25,64 @@ export class CreateProjectLoader {
 
     // scope values
     this.scope = {
-      step: '=cheStep'
+      model: '=cheModel'
     };
+  }
+
+  link ($scope, element) {
+    let craneEl = element.find('.create-project-loader'),
+      cargoEl = element.find('#load'),
+      workingLogEl = element.find('.working-log'),
+      oldSteps = [],
+      newStep;
+    $scope.$watch(() => {return $scope.model.getCurrentProgressStep();}, (newVal,oldVal) => {
+      oldSteps.push(oldVal);
+      newStep = newVal;
+
+      // animation initialization
+      if (newVal===1){
+        craneEl.addClass('step-' + newVal);
+        cargoEl.addClass('layer-' + newVal);
+      }
+
+      if (oldVal===newVal) {
+        return;
+      }
+      let stages = element.find('.stage'),
+        outputs = stages.parent();
+
+      stages.removeClass('active');
+      outputs.find('.stage-' + newVal).addClass('active');
+
+      if (workingLogEl.hasClass('untouched')) {
+        stages.removeClass('opened');
+        outputs.find('.stage-' + newVal).addClass('opened');
+      }
+    });
+
+    // event fires on animation iteration end
+    element.find('.anim.trolley-block').bind('animationiteration', () => {
+      for (let i=0; i<oldSteps.length; i++) {
+        craneEl.removeClass('step-'+oldSteps[i]);
+        cargoEl.removeClass('layer-'+oldSteps[i]);
+      }
+      oldSteps.length = 0;
+
+      craneEl.addClass('step-'+newStep);
+      cargoEl.addClass('layer-'+newStep);
+    });
+
+    // manual switching stages
+    workingLogEl.bind('click', (event) => {
+      let targetEl = angular.element(event.target);
+      if (!targetEl.hasClass('stage-title')) {
+        return false;
+      }
+      let selectedStageEl = targetEl.parent(),
+        stages = workingLogEl.find('.stage');
+      workingLogEl.removeClass('untouched');
+      stages.removeClass('opened');
+      selectedStageEl.addClass('opened');
+    });
   }
 }
